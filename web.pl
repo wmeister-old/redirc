5488f847c61e7e14ab779fb68234d63779764911
Willaim Meister <wmeister86@gmail.com>
2011-10-14 13:03:51 -0400

    turn url values into links

--- web.pl
+++ web.pl
@@ -1,7 +1,14 @@
 #!/usr/bin/perl
 use Redis;
+use URI::Escape;
 
 my $r = Redis->new;
+
 print qq{Content-type: text/html\n\n<html><body><table width="100%" border="1"><tr><td><b>key</b></td><td><b>value</b></td></tr>};
-foreach my $k ($r->keys('*')) {	my $v = $r->get($k); print "<tr><td>$k</td><td>$v</td></tr>"; }
+
+foreach my $k (sort $r->keys('*')) {	
+		my $v = $r->get($k);
+		$v = '<a href="'.uri_escape($v).qq[" target="_blank">$v</a>] if $v =~ /^http:\/\//;
+		print "<tr><td>$k</td><td>$v</td></tr>"; 
+}
 print "</table></body></html>";
